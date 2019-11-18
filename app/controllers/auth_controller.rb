576c1b21004278af6191f8e904d0989131276a10
Alex Mendes <mendes.develop@gmail.com>
2019-11-18 14:55:21 -0500

    First commit: /login, /signup, /profile

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,32 @@
+class AuthController < ApplicationController
+
+    def login
+        # 1- Find user based on username and/or email
+        # 2- check if user exist and password matches
+        # 3- if t's true generate a JWT token else render errors
+
+        # 3.a create payload, encode it with JWT.econde + SecretKey + HMAC Alghorithm
+        # 3.b render token to the user render {token: token}
+
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            # payload = {user_id: user.id}
+            # token = JWT.encode(payload, 'SecretKey', 'HS256')
+            render json: {token: create_token(user.id)} 
+        else
+            render json: {errors: ["Password or User don't match. Please, try it again."]}, status: 422
+        end
+       
+    end
+
+    
+
+    # def is_authenticated(params)
+    #     user = User.find_by(username: params[:username])
+    #     if user !nil 
+    #         return user.authenticate(params[:password])
+    #     end
+    #     return false
+    # end 
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+    def profile
+        # 1- extracts token from request.headers["Authorization"]
+        # 2- Decode token and extracts basic valuable information lie user_id
+        # 3- Send it back to the front end as json:
+
+        # token = request.headers['Authorization'].split(' ')[1]  ##Split Bearer from the actual token
+        # decode_token = JWT.decode(token, 'SecretKey', true, {algorithm: 'HS256'})
+        # user_id = decode_token[0]['user_id']
+        # current_user = User.find(user_id)
+
+        #code above got subtitute by helper methods
+
+        render json: current_user
+    end
+
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            render json: {token: create_token(user.id)}
+        else
+           render json: {errors: user.errors.full_messages}, status: 422
+        end
+        
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+
+
+
+end
