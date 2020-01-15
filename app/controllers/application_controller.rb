caa6ac1962f0cd1c80daa51ead1a9f7576a78437
Alex Mendes <mendes.develop@gmail.com>
2020-01-14 22:32:07 -0500

    enviroment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
     end
 
     def secretKey 
-        jwt_secret = process.env['JWT_SECRET']
+        jwt_secret = 'SecretKey' #process.env['JWT_SECRET']
         puts jwt_secret
     end
 
